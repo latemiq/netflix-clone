4ef2cb9d440beff20cae134db1ea4e136b6cc906
Latemiq <dzierzkowskij@gmail.com>
2023-03-16 19:59:22 +0100

    ADD: request.js

---
+++ netflix-clone/src/request.js
@@ -0,0 +1,14 @@
+const API_KEY = "7b3bd2752100eae35ee72c511fca3531";
+
+const requests ={
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&languange=en-US`,
+    fetchNetflixOrginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&languange=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentariesMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+} 
+
+export default request;
